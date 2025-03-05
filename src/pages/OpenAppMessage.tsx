73e1b178b8456ab1fe878277252bd1baa3b864f1
Marcos Celiz <mceliz@gmail.com>
2025-03-05 12:33:34 -0300

    Se modifica traduccion a pantalla de confirmacion de cambio de password

--- src/pages/OpenAppMessage.tsx
+++ src/pages/OpenAppMessage.tsx
@@ -8,8 +8,8 @@ const OpenAppMessage = () => {
     <div className="open-app-message">
       <img src={logo} alt="Abre tu aplicación" className="app-image" />
       <img src={logo2} alt="Abre tu aplicación" className="app-image" />
-      <h1>¡Contraseña Restablecida!</h1>
-      <p>Tu contraseña ha sido restablecida con éxito.</p><p>Abre tu aplicación para continuar.</p> 
+      <h1>Password Reset!</h1>
+      <p>Your password has been successfully reset.</p><p>Open your app to continue.</p>
       {/* <Link to="/" className="back-home-button">Volver al inicio</Link> */}
     </div>
   );
